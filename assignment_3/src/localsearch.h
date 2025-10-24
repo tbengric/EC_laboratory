662db7a7c0881e78a8592f9298a750bf3d5c06ba
Tidiane Bengriche <168076434+tbengric@users.noreply.github.com>
2025-10-24 13:56:13 +0200

    Define function prototypes in localsearch.h
    Added function prototypes for local search algorithms.

--- assignment_3/src/localsearch.h
+++ assignment_3/src/localsearch.h
@@ -5,3 +5,25 @@
 #include <random>
 #include "heuristics.h"
 #include "distance_matrix.h"
+
+using namespace std;
+
+double delta_intra_node(const vector<int>& path, int i, int j, const vector<vector<int>>& dist_matrix, const vector<Node>& nodes) {
+    
+}
+
+double delta_intra_edge(const vector<int>& path, int i, int j, const vector<vector<int>>& dist_matrix, const vector<Node>& nodes) {
+    
+}
+
+double delta_inter_node(const vector<int>& path, int i, int j, const vector<vector<int>>& dist_matrix, const vector<Node>& nodes) {
+    
+}
+
+pair<vector<int>, int> steepest_local_search(const vector<int>& initial_path, double initial_cost, const vector<vector<int>>& dist_matrix, const string& intra_method) {
+    
+}
+
+pair<vector<int>, int> greedy_local_search(const vector<int>& initial_path, double initial_cost, const vector<vector<int>>& dist_matrix, const string& intra_method) {
+    
+}
