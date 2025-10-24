2d78478a218af1a144bc9bc5f5177d6bd739737e
Tidiane Bengriche <168076434+tbengric@users.noreply.github.com>
2025-10-24 15:47:44 +0200

    Add delta_intra_node function implementation
    Implemented delta_intra_node function to calculate cost changes for intra-node swaps.

--- assignment_3/src/localsearch.h
+++ assignment_3/src/localsearch.h
@@ -8,8 +8,39 @@
 
 using namespace std;
 
-double delta_intra_node(const vector<int>& path, int i, int j, const vector<vector<int>>& dist_matrix, const vector<Node>& nodes) {
-    
+double delta_intra_node(const vector<int>& path, int firstnodeindex, int secondnodeindex, const vector<vector<int>>& dist_matrix, const vector<Node>& nodes) {
+    int path_size = path.size();
+    if (path_size < 2) return 0.0;
+
+    int prev_first = path[(firstnodeindex - 1 + path_size) % path_size];
+    int first = path[firstnodeindex];
+    int next_first = path[(firstnodeindex + 1) % path_size];
+
+    int prev_second = path[(secondnodeindex - 1 + path_size) % path_size];
+    int second = path[secondnodeindex];
+    int next_second = path[(secondnodeindex + 1) % path_size];
+
+    double old_cost = 0.0;
+    double new_cost = 0.0;
+
+    if ((firstnodeindex + 1) % path_size == secondnodeindex) {
+        // adjacent nodes
+        old_cost += dist_matrix[prev_first][first] + dist_matrix[first][second] + dist_matrix[second][next_second];
+        new_cost += dist_matrix[prev_first][second] + dist_matrix[second][first] + dist_matrix[first][next_second];
+    } else if ((secondnodeindex + 1) % path_size == firstnodeindex) {
+        // adjacent nodes
+        old_cost += dist_matrix[prev_second][second] + dist_matrix[second][first] + dist_matrix[first][next_first];
+        new_cost += dist_matrix[prev_second][first] + dist_matrix[first][second] + dist_matrix[second][next_first];
+    } else {
+        // non-adjacent nodes
+        old_cost += dist_matrix[prev_first][first] + dist_matrix[first][next_first];
+        old_cost += dist_matrix[prev_second][second] + dist_matrix[second][next_second];
+
+        new_cost += dist_matrix[prev_first][second] + dist_matrix[second][next_first];
+        new_cost += dist_matrix[prev_second][first] + dist_matrix[first][next_second];
+    }   
+    double delta = new_cost - old_cost;
+    return delta;
 }
 
 double delta_intra_edge(const vector<int>& path, int i, int j, const vector<vector<int>>& dist_matrix, const vector<Node>& nodes) {
